7f782471bd83548449d11cbea37314c1889f4987
mariam <mariam@kompishka.local>
2025-12-09 15:15:07 +0400

    Add product class

---
+++ src/main/java/com/example/ecommerce/model/Product.java
@@ -0,0 +1,55 @@
+package com.example.ecommerce.model;
+
+public class Product {
+
+    private Long id;
+
+    private String name;
+
+    private double price;
+
+    private int stockQuantity;
+
+    public Product() {
+
+    }
+
+    public Product(Long id, String name, double price, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
